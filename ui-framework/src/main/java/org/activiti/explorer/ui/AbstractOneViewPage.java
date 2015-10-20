175301309ad837ed1e88259e9c6a8243b62f4398
Marlon Richert <marlon@vaadin.com>
2015-10-20 18:58:15 +0300

    refactor main layout

--- ui-framework/src/main/java/org/activiti/explorer/ui/AbstractOneViewPage.java
+++ ui-framework/src/main/java/org/activiti/explorer/ui/AbstractOneViewPage.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,6 +12,7 @@
  */
 package org.activiti.explorer.ui;
 
+import com.vaadin.ui.VerticalLayout;
 import org.activiti.explorer.ui.custom.ToolBar;
 
 import com.vaadin.ui.Component;
@@ -20,13 +21,13 @@ import com.vaadin.ui.GridLayout;
 
 /**
  * Superclass for one view pages
- * 
+ *
  * @author Tijs Rademakers
  */
 public abstract class AbstractOneViewPage extends AbstractPage {
 
   private static final long serialVersionUID = 1L;
-  
+
   /**
    * Override this method (and call super()) when you want to influence the UI.
    */
@@ -40,46 +41,30 @@ public abstract class AbstractOneViewPage extends AbstractPage {
    * Subclasses are expected to provide their own menuBar.
    */
   protected void addMenuBar() {
-    
+
     // Remove any old menu bar
     String activeEntry = null;
     if (toolBar != null) {
       activeEntry = toolBar.getCurrentEntryKey();
       grid.removeComponent(toolBar);
     }
-    
+
     // Create menu bar
     ToolBar menuBar = createMenuBar();
     if (menuBar != null) {
       toolBar = createMenuBar();
-      grid.addComponent(toolBar, 0, 0);
-      
+      grid.addComponent(toolBar);
+
       if (activeEntry != null) {
         toolBar.setActiveEntry(activeEntry);
       }
     }
   }
-  
+
   protected void addMainLayout() {
-    grid = new GridLayout(1, 2);
+    grid = new VerticalLayout();
     grid.setSizeFull();
-    
-    // Height division
-    grid.setRowExpandRatio(1, 1.0f);
-    
+
     setCompositionRoot(grid);
-  }
-  
-  protected void setDetailComponent(Component detail) {
-    if (grid.getComponent(0, 1) != null) {
-      grid.removeComponent(0, 1);
-    }
-    if (detail != null) {
-      grid.addComponent(detail, 0, 1);
-    }
-  }
-  
-  protected Component getDetailComponent() {
-    return grid.getComponent(0, 1);
   }
 }
--- ui-framework/src/main/java/org/activiti/explorer/ui/AbstractPage.java
+++ ui-framework/src/main/java/org/activiti/explorer/ui/AbstractPage.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,43 +12,38 @@
  */
 package org.activiti.explorer.ui;
 
-import org.activiti.explorer.ui.custom.ToolBar;
-
-import com.vaadin.ui.AbstractSelect;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.*;
 import com.vaadin.ui.themes.Reindeer;
+import org.activiti.explorer.ui.custom.ToolBar;
 
 
 /**
  * Superclass for all Explorer pages
- * 
- * @author Joram Barrez
- * @author Frederik Heremans
  */
 public abstract class AbstractPage extends CustomComponent {
 
   private static final long serialVersionUID = 1L;
-  
+
   protected ToolBar toolBar;
-  protected GridLayout grid;
+  protected VerticalLayout grid;
   protected AbstractSelect select;
   protected boolean showEvents;
-  
+  private HorizontalSplitPanel outerSplitPanel = new HorizontalSplitPanel();
+  private HorizontalSplitPanel innerSplitPanel = new HorizontalSplitPanel();
+
   // Overriding attach(), so we can construct the components first, before the UI is built,
   // that way, all member fields of subclasses are initialized properly
   @Override
   public void attach() {
    initUi();
   }
-  
+
   /**
    * Override this method (and call super()) when you want to influence the UI.
    */
   protected void initUi() {
     showEvents = getEventComponent() != null;
-    
+
     addMainLayout();
     setSizeFull();
     addMenuBar();
@@ -58,76 +53,78 @@ public abstract class AbstractPage extends CustomComponent {
      addEventComponent();
     //}
   }
-  
+
   protected void addEventComponent() {
 	Component eventComponent = getEventComponent();
-	if (eventComponent !=null)
-    grid.addComponent(eventComponent, 2, 0, 2, 2);
+	if (eventComponent !=null) {
+      eventComponent.setId("event-component");
+      innerSplitPanel.setSecondComponent(eventComponent);
+    }
   }
 
   /**
    * Subclasses are expected to provide their own menuBar.
    */
   protected void addMenuBar() {
-    
+
     // Remove any old menu bar
     String activeEntry = null;
     if (toolBar != null) {
       activeEntry = toolBar.getCurrentEntryKey();
       grid.removeComponent(toolBar);
     }
-    
+
     // Create menu bar
     ToolBar menuBar = createMenuBar();
     if (menuBar != null) {
       toolBar = createMenuBar();
-      grid.addComponent(toolBar, 0, 0 , 1, 0);
-      
+      toolBar.setId("tool-bar");
+      grid.addComponent(toolBar);
+      grid.setExpandRatio(toolBar, 0);
+
       if (activeEntry != null) {
         toolBar.setActiveEntry(activeEntry);
       }
     }
   }
-  
+
   public ToolBar getToolBar() {
     return toolBar;
   }
-  
-  protected abstract ToolBar createMenuBar();
-  
-  protected void addMainLayout() {
-    if(showEvents) {
-      grid = new GridLayout(3, 3);
-      grid.setColumnExpandRatio(0, .20f);
-      grid.setColumnExpandRatio(1, .40f);
-      grid.setColumnExpandRatio(2, .40f);
-    } else {
-      grid = new GridLayout(2, 3);
 
-      grid.setColumnExpandRatio(0, .25f);
-      grid.setColumnExpandRatio(1, .75f);
-    }
-    
+  protected abstract ToolBar createMenuBar();
+
+  protected void addMainLayout() {
+    grid = new VerticalLayout();
+
     grid.addStyleName(Reindeer.SPLITPANEL_SMALL);
     grid.setSizeFull();
-    
-    // Height division
-    grid.setRowExpandRatio(2, 1.0f);
-    
-    setCompositionRoot(grid);
+
+    setCompositionRoot(outerSplitPanel);
+    outerSplitPanel.setFirstComponent(grid);
+    outerSplitPanel.setSecondComponent(innerSplitPanel);
+
+    outerSplitPanel.setSplitPosition(20, Unit.PERCENTAGE);
+
+    outerSplitPanel.setId("outer-split-panel");
+    innerSplitPanel.setId("inner-split-panel");
   }
-  
+
   protected void addSearch() {
     Component searchComponent = getSearchComponent();
     if(searchComponent != null) {
-      grid.addComponent(searchComponent, 0, 1);
+      searchComponent.setId("search-component");
+      grid.addComponent(searchComponent);
+      grid.setExpandRatio(searchComponent, 0);
     }
   }
-  
+
   protected void addSelectComponent() {
     AbstractSelect select = createSelectComponent();
     if (select != null) {
-      grid.addComponent(select, 0, 2);
+      select.setId("select");
+      grid.addComponent(select);
+      grid.setExpandRatio(select, 1);
     }
   }
 
@@ -137,31 +134,32 @@ public abstract class AbstractPage extends CustomComponent {
    * allowing to select elements from eg. a list, tree, etc.
    */
   protected abstract AbstractSelect createSelectComponent();
-  
+
   /**
    * Refreshes the elements of the list, and selects the next
    * one (useful when the selected element is deleted).
    */
   public abstract void refreshSelectNext();
-  
+
   /**
    * Select a specific element from the selection component.
    */
   public abstract void selectElement(int index);
-  
+
   protected void setDetailComponent(Component detail) {
-    if(grid.getComponent(1, 1) != null) {
-      grid.removeComponent(1, 1);
+    if(innerSplitPanel.getFirstComponent() != null) {
+      innerSplitPanel.setFirstComponent(null);
     }
     if(detail != null) {
-      grid.addComponent(detail, 1, 1, 1, 2);
+      detail.setId("detail");
+      innerSplitPanel.setFirstComponent(detail);
     }
   }
-  
+
   protected Component getDetailComponent() {
-    return grid.getComponent(1, 0);
+    return innerSplitPanel.getFirstComponent();
   }
-  
+
   /**
    * Override to get the search component to display above the table. Return null
    * when no search should be displayed.
@@ -169,15 +167,15 @@ public abstract class AbstractPage extends CustomComponent {
   public Component getSearchComponent() {
     return null;
   }
-  
+
   /**
-   * Get the component to display the events in. 
-   * 
+   * Get the component to display the events in.
+   *
    * Return null by default: no event-component will be used,
    * in that case the main UI will be two columns instead of three.
-   * 
+   *
    * Override in case the event component must be shown:
-   * three columns will be used then. 
+   * three columns will be used then.
    */
   protected Component getEventComponent() {
     return null;
